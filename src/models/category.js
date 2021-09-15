2f1c68136898f9d9abe906e989fcf9a2d73db3f4
sergeyivanchik <ivanchik-s@mail.ru>
2021-09-15 22:44:00 +0300

    update

--- src/models/category.js
+++ src/models/category.js
@@ -3,7 +3,8 @@ const { Schema } = mongoose
 
 const schema = new Schema({
   title:{ type: String, required: true },
-  image: { type: String, required: true }
+  image: { type: String, required: true },
+  description: { type: String, required: true },
 },
 {
   versionKey: false
